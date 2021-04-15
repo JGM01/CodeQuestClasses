511453ba07651791c5701d38432461e7c2e688e3
harrislegobrick <54484642+harrislegobrick@users.noreply.github.com>
2021-04-15 18:11:54 -0400

    Added documentation

--- TwoDeeHelper.java
+++ TwoDeeHelper.java
@@ -2,6 +2,16 @@ public class TwoDeeHelper {
     private TwoDeeHelper() {
     }
 
+    /**
+     *  Helper method that will return how many adjacent things are to a location
+     * @param board the board being tested
+     * @param x the x location being tested
+     * @param y the y location being testd
+     * @param flag the thing that you are looking to detect
+     * @param boardSizeX the size of the board in the X direction. This prevents IndexOutOfBounds exceptions.
+     * @param boardSizeY the size of the board in the Y direction. This prevents IndexOutOfBounds exceptions.
+     * @return
+     */
     public static int adjacent(int[][] board, int x, int y, int flag, int boardSizeX, int boardSizeY) {
         int numAdj = 0;
         for (int y2 = y - 1; y2 < y + 2; y2++) {
