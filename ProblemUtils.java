1c1d5336b7274b300d3fe51a8f239c8d1288586c
harrislegobrick <54484642+harrislegobrick@users.noreply.github.com>
2021-04-17 16:41:01 -0400

    Formatted

--- ProblemUtils.java
+++ ProblemUtils.java
@@ -10,8 +10,11 @@ public class ProblemUtils {
     }
 
     /**
-     * Basically a wrapped for loop. It will run the logic "X" amount of times where "X" is the first number entered into {@code System.in} (the standard input stream).
-     * @param sc scanner to get the first line entered
+     * Basically a wrapped for loop. It will run the logic "X" amount of times where
+     * "X" is the first number entered into {@code System.in} (the standard input
+     * stream).
+     * 
+     * @param sc    scanner to get the first line entered
      * @param logic the logic that will run "X" amount of times
      */
     public static void testCases(Scanner sc, Runnable logic) {
