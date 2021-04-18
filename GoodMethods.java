7742f1ca47c7835a2c1bd7cd4108bb6ca8d37294
JGM01 <41394708+JGM01@users.noreply.github.com>
2021-04-17 22:03:38 -0400

    lots of stuff

--- GoodMethods.java
+++ GoodMethods.java
@@ -1,8 +1,26 @@
 import java.util.*;
 
 public class GoodMethods {
+    public static OBJECT obj = new OBJECT();
+    public static void ListMethods() {
+        LinkedList list = new LinkedList();
+        LinkedList list2 = new LinkedList();
+        
+        list.addAll(list2);
+        
+        list.clear();
 
-    
+        list2 = (LinkedList) list.clone();
 
+        list.contains(obj);
+
+        
+    }
+
+
+
+}
+class OBJECT {
+    public OBJECT(){}
 
 }
