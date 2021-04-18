fad78022507eea7527d57bba3b1f98ce7da32812
harrislegobrick <54484642+harrislegobrick@users.noreply.github.com>
2021-04-18 16:27:29 -0400

    Added more to the switch expression pratice

--- SwitchExpressionPractice.java
+++ SwitchExpressionPractice.java
@@ -4,7 +4,8 @@ public class SwitchExpressionPractice {
     public static Scanner sc = new Scanner(System.in);
     public static void main(String[] args) {
         String s = switch(Integer.parseInt(sc.nextLine())) {
-            case 1 -> "lol";
+            // You can have multiple things in each switch expressions
+            case 1, 2, 3 -> "lol";
             default -> "LOOOLLL";
         };
         System.out.println(s);
